9cc5f22368b125a01d7a2778d7ffa3bf6498d63d
WindWaker991 <83779081+WindWaker991@users.noreply.github.com>
2022-06-08 00:34:29 -0400

    Creacion del CMake.txt

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: E:/Repos VS Code/ED22-01-Angel-Rivera
+# Build directory: E:/Repos VS Code/ED22-01-Angel-Rivera/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
