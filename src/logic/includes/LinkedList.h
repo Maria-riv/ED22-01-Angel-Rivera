45f9fcdba348e82ab21827f706d98735403c9e33
WindWaker991 <83779081+WindWaker991@users.noreply.github.com>
2022-07-13 22:27:23 -0400

    creacion menu y cambios varios

--- src/logic/includes/LinkedList.h
+++ src/logic/includes/LinkedList.h
@@ -1,29 +1,80 @@
-#ifndef LINKEDLIST_H
-#define LINKEDLIST_H
- 
-#include <fstream>
-#include <iostream>
-#include <string>
-#include <stdlib.h>
- 
-#include "Nodo.h"
- 
-using namespace std;
- 
- 
-class LinkedList
-{
-    public:
-        LinkedList();
-        ~LinkedList();
+#ifndef LIST_H
+#define LIST_H
 
-        void agregar(Persona);
-        int size();
-        Nodo* buscar(int);
- 
-    private:
-        Nodo *head;
-        int num_nodes;
+#include <iostream>
+
+template <typename T>
+class List
+{
+    class Node;
+
+    Node *head, *tail;
+    int mSize;
+
+public:
+    List();
+
+    class Iterator;
+
+    Iterator begin() { return Iterator(head); }
+
+    Iterator end() { return Iterator(nullptr); }
+
+    int size();
+
+    bool empty();
+
+    void push_back(T &data);
+
+    T &get(int i);
+
+    T &operator[](int i);
+
+    void print();
+
+    friend std::ostream &operator<<(std::ostream &os, List<T> &list) {
+        for (auto it : list) {
+            os << it << " ";
+        }
+        return os;
+    }
+
+private:
+    class Node
+    {
+    public:
+        T data;
+        Node *next;
+
+        Node(T &data) : data(data), next(nullptr) {}
+    };
+
+public:
+    class Iterator
+    {
+        Node *current;
+
+    public:
+        Iterator() : current(nullptr) {}
+
+        Iterator(Node *node) : current(node) {}
+
+        void operator=(Iterator it) {
+            current = it.current;
+        }
+
+        bool operator!=(Iterator it) {
+            return current != it.current;
+        }
+
+        void operator++() {
+            current = current->next;
+        }
+
+        T &operator*() {
+            return current->data;
+        }
+    };
 };
- 
-#endif 
+
+#endif // LIST_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "logic/includes/CentroidTracker.h"
+#include "logic/includes/LinkedList.h"
+#include "logic/includes/Persona.h"
 #include <opencv2/dnn.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
@@ -12,55 +14,57 @@ string CLASSES[] = {"background", "aeroplane", "bicycle", "bird", "boat",
 	"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
 	"sofa", "train", "tvmonitor"};
 
-int main() {
-    std::cout << "Hello, Tracker!" << std::endl;
+void watchVideo1(){
     auto centroidTracker = new CentroidTracker(20);
-
-    VideoCapture cap(0);
-//    VideoCapture cap("../../test2.mp4");
-    if (!cap.isOpened()) {
-        cout << "Cannot open camera";
-    }
+    vector<vector<int>> trackers;
+    List<Persona> trackableObjects;
+    VideoCapture cap("Docs/example_01.mp4");
 
     String modelTxt = "E:/Repos VS Code/Centroid-Object-Tracking/model/deploy.prototxt";
     String modelBin = "E:/Repos VS Code/Centroid-Object-Tracking/model/res10_300x300_ssd_iter_140000.caffemodel";
 
-    cout << "Loading model.." << endl;
     auto net = dnn::readNetFromCaffe(modelTxt, modelBin);
 
-    cout << "Starting video stream" << endl;
+    int totalFrames = 0;
+    int totalUp = 0;
+    int totalDown = 0;
+    int skipFrames = 50;
+
     while (cap.isOpened()) {
         Mat cameraFrame;
-        cap.read(cameraFrame);
+        cap.read(cameraFrame); 
 
         resize(cameraFrame, cameraFrame, Size(400, 300));
-        auto inputBlob = dnn::blobFromImage(cameraFrame, 1.0, Size(400, 300), Scalar(104.0, 177.0, 123.0));
 
-        net.setInput(inputBlob);
-        auto detection = net.forward("detection_out");
-        Mat detectionMat(detection.size[2], detection.size[3], CV_32F, detection.ptr<float>());
+        vector<vector<int>> rects;
+        
+        if(totalFrames%skipFrames == 0){
+            trackers.clear();
 
-        vector<vector<int>> boxes;
+            auto inputBlob = dnn::blobFromImage(cameraFrame, 1.0, Size(400, 300), Scalar(104.0, 177.0, 123.0));
+            net.setInput(inputBlob);
+            auto detection = net.forward("detection_out");
+            Mat detectionMat(detection.size[2], detection.size[3], CV_32F, detection.ptr<float>());
 
-        float confidenceThreshold = 0.2;
-        for (int i = 0; i < detectionMat.rows; i++) {
-            float confidence = detectionMat.at<float>(i, 2);
+            float confidenceThreshold = 0.2;
+            for (int i = 0; i < detectionMat.rows; i++) {
+                float confidence = detectionMat.at<float>(i, 2);
 
-            if (confidence > confidenceThreshold) {
-                int xLeftTop = static_cast<int>(detectionMat.at<float>(i, 3) * cameraFrame.cols);
-                int yLeftTop = static_cast<int>(detectionMat.at<float>(i, 4) * cameraFrame.rows);
-                int xRightBottom = static_cast<int>(detectionMat.at<float>(i, 5) * cameraFrame.cols);
-                int yRightBottom = static_cast<int>(detectionMat.at<float>(i, 6) * cameraFrame.rows);
+                if (confidence > confidenceThreshold) {
+                    int xLeftTop = static_cast<int>(detectionMat.at<float>(i, 3) * cameraFrame.cols);
+                    int yLeftTop = static_cast<int>(detectionMat.at<float>(i, 4) * cameraFrame.rows);
+                    int xRightBottom = static_cast<int>(detectionMat.at<float>(i, 5) * cameraFrame.cols);
+                    int yRightBottom = static_cast<int>(detectionMat.at<float>(i, 6) * cameraFrame.rows);
 
-                Rect object((int) xLeftTop, (int) yLeftTop, (int) (xRightBottom - xLeftTop),
-                            (int) (yRightBottom - yLeftTop));
-                rectangle(cameraFrame, object, Scalar(0, 255, 0), 2);
+                    Rect object((int) xLeftTop, (int) yLeftTop, (int) (xRightBottom - xLeftTop),
+                                (int) (yRightBottom - yLeftTop));
 
-                boxes.insert(boxes.end(), {xLeftTop, yLeftTop, xRightBottom, yRightBottom});
-            }
+                    trackers.insert(trackers.end(), {xLeftTop, yLeftTop, xRightBottom, yRightBottom});
+                }
+            } 
         }
 
-        auto objects = centroidTracker->update(boxes);
+        auto objects = centroidTracker->update(rects);
 
         if (!objects.empty()) {
             for (auto obj: objects) {
@@ -70,18 +74,6 @@ int main() {
                             FONT_HERSHEY_COMPLEX, 0.5, Scalar(0, 255, 0), 2);
             }
 
-            //drawing the path
-            // for (auto obj: objects) {
-            //     int k = 1;
-            //     for (int i = 1; i < centroidTracker->path_keeper[obj.first].size(); i++) {
-            //         int thickness = int(sqrt(20 / float(k + 1) * 2.5));
-            //         cv::line(cameraFrame,
-            //                  Point(centroidTracker->path_keeper[obj.first][i - 1].first, centroidTracker->path_keeper[obj.first][i - 1].second),
-            //                  Point(centroidTracker->path_keeper[obj.first][i].first, centroidTracker->path_keeper[obj.first][i].second),
-            //                  Scalar(0, 0, 255), thickness);
-            //         k += 1;
-            //     }
-            // }
         }
         imshow("Detection", cameraFrame);
         char c = (char) waitKey(1);
@@ -89,5 +81,119 @@ int main() {
             break;
     }
     delete centroidTracker;
+}
+
+void watchVideo2(){
+    auto centroidTracker = new CentroidTracker(20);
+    vector<vector<int>> trackers;
+    List<Persona> trackableObjects;
+    VideoCapture cap(0);
+
+    String modelTxt = "E:/Repos VS Code/Centroid-Object-Tracking/model/deploy.prototxt";
+    String modelBin = "E:/Repos VS Code/Centroid-Object-Tracking/model/res10_300x300_ssd_iter_140000.caffemodel";
+
+    auto net = dnn::readNetFromCaffe(modelTxt, modelBin);
+
+    int totalFrames = 0;
+    int totalUp = 0;
+    int totalDown = 0;
+    int skipFrames = 50;
+
+    while (cap.isOpened()) {
+        Mat cameraFrame;
+        cap.read(cameraFrame); 
+
+        resize(cameraFrame, cameraFrame, Size(400, 300));
+
+        vector<vector<int>> rects;
+        
+        if(totalFrames%skipFrames == 0){
+            trackers.clear();
+
+            auto inputBlob = dnn::blobFromImage(cameraFrame, 1.0, Size(400, 300), Scalar(104.0, 177.0, 123.0));
+            net.setInput(inputBlob);
+            auto detection = net.forward("detection_out");
+            Mat detectionMat(detection.size[2], detection.size[3], CV_32F, detection.ptr<float>());
+
+            float confidenceThreshold = 0.2;
+            for (int i = 0; i < detectionMat.rows; i++) {
+                float confidence = detectionMat.at<float>(i, 2);
+
+                if (confidence > confidenceThreshold) {
+                    int xLeftTop = static_cast<int>(detectionMat.at<float>(i, 3) * cameraFrame.cols);
+                    int yLeftTop = static_cast<int>(detectionMat.at<float>(i, 4) * cameraFrame.rows);
+                    int xRightBottom = static_cast<int>(detectionMat.at<float>(i, 5) * cameraFrame.cols);
+                    int yRightBottom = static_cast<int>(detectionMat.at<float>(i, 6) * cameraFrame.rows);
+
+                    Rect object((int) xLeftTop, (int) yLeftTop, (int) (xRightBottom - xLeftTop),
+                                (int) (yRightBottom - yLeftTop));
+
+                    trackers.insert(trackers.end(), {xLeftTop, yLeftTop, xRightBottom, yRightBottom});
+                }
+            } 
+        }
+
+        auto objects = centroidTracker->update(rects);
+
+        if (!objects.empty()) {
+            for (auto obj: objects) {
+                circle(cameraFrame, Point(obj.second.first, obj.second.second), 4, Scalar(255, 0, 0), -1);
+                string ID = std::to_string(obj.first);
+                cv::putText(cameraFrame, ID, Point(obj.second.first - 10, obj.second.second - 10),
+                            FONT_HERSHEY_COMPLEX, 0.5, Scalar(0, 255, 0), 2);
+            }
+
+        }
+        imshow("Detection", cameraFrame);
+        char c = (char) waitKey(1);
+        if (c == 27)
+            break;
+    }
+    delete centroidTracker;
+}
+
+int main() {
+    string opcion3;
+    int countType;
+    cout << "Bienvenido al sistema de vigilancia de ACME\n" << "\nDesea inicar sesion?\n" << endl;
+    cin >> opcion3;
+    while (opcion3 == "SI" || opcion3 == "si" || opcion3 == "Si" || opcion3 == "sI") {
+        int opcion;
+        cout << "Como desea iniciar sesion?\n" << "\n(1) Iniciar como ADMINISTRADOR\n" << "(2) Iniciar como GUARDIA" << "\n(3) Cerrar Sesion\n" << endl;
+        cin >> opcion;
+        switch(opcion){
+            case 1:
+                cout << "Bienvenido al menu ADMIN" << endl << "Seleccione como se va a trabajar con el conteo de personas: \n(1) Conteo con un video \n(2) Conteo con la camara";
+                int eleccion;
+                cin >> eleccion;
+                while(eleccion != 1 || eleccion !=2){
+                    cout << "Operacion no valida, intente de nuevo" << endl;
+                    cin >> eleccion;
+                }
+                if(eleccion == 1){
+                    countType = 1;
+                }
+                else{
+                    countType = 2;
+                }
+                break;
+            case 2:
+                cout << "Bienvenido al menu GUARDIA" << endl << "¿Desea empezar con el conteo de personas?\n";
+                string opcion4;
+                cin >> opcion4;
+                if(opcion4 == "Si" || opcion4 == "SI" || opcion4 == "si"){
+                    if(countType == 1){
+                        watchVideo1();
+                    }
+                    else{
+                        watchVideo2();
+                    }
+                }
+                break;
+        }
+        cout << "¿Desea realizar otra accion?" << endl;
+        cin >> opcion3;
+    }
+    cout << "Cerrando sesion...";
     return 0;
 }
